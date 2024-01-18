db1ffb34d15687bf0b535f231a97949b2d38a580
Nikhil Goyal <135103317+ngoyal88@users.noreply.github.com>
2024-01-18 11:57:42 +0530

    Update campSchedule.php

--- campSchedule.php
+++ campSchedule.php
@@ -174,6 +174,12 @@
           </div>
     </form>   
 </div>
+    <div class="container CSchedule" ></div>
+<script
+  src="https://code.jquery.com/jquery-3.7.1.js"
+  integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+  crossorigin="anonymous"></script>
+  <script src="district.js"></script>
 
 <!-- Footer -->
 <!-- ================================================================================================================================================================================================================================== -->
